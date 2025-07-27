4901db7ce0bb7678e560ba0387ef77efa75eeba8
vishalsharma070901 <shobits723@gmail.com>
2025-07-27 22:36:07 +0530

    first java program

---
+++ FirstClass.java
@@ -0,0 +1,7 @@
+class FirstClass {
+    public static void main(String[] args) {
+        //  for printing the output, three use the below line also to use this we can use sout also
+        System.out.print("Hello World");
+        
+    }
+}
