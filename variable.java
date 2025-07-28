93dacac9ea1532babb8edebf4e0f6c86d7cd7395
vishalsharma070901 <shobits723@gmail.com>
2025-07-29 00:32:19 +0530

    learning variables

---
+++ variable.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class variable {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+        sc.close();
+    }
+    
+}
+
+// In Java, there are two types of data types
+// 1. Primitive Data Types
+// 2. Non-Primitive Data Types
+
+// Primitive Data Types
+// 1. int
+// 2. float
+// 3. double
+// 4. char
+// 5. boolean
+// 6. byte
+// 7. short
+// 8. long
+
+
+// Non-Primitive Data Types
+// 1. String
+// 2. Array
+// 3. Class
+// 4. Interface
+// 5. Enum
+// 6. Annotation
+// 7. Record
+// 8. Sealed Class
+// 9. Sealed Interface
+
+
+//take the input there are many types of scanner functions
+// next --> it will take the input until the space is encountered
+// nextLine() --> it will take the input until the new line is encountered
+// nextInt() --> it will take the input as an integer
+// nextFloat() --> it will take the input as a float
+// nextDouble() --> it will take the input as a double
+// nextBoolean() --> it will take the input as a boolean
+// nextByte() --> it will take the input as a byte
+// nextShort() --> it will take the input as a short
+// nextLong() --> it will take the input as a long
