daff3ca2e554e12a6b242f2940141539acd94156
sharmav3 <vishal.sharma@autodesk.com>
2025-08-01 01:01:50 +0530

    working on functions

--- Functions.java
+++ Functions.java
@@ -6,7 +6,8 @@ class Functions {
         System.out.println(name);
     }
 
-
+ 
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
